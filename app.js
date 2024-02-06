4fb5909227d84c93fad888132b8e388077fddfed
Vishnu Kadam <vishnukadam459@gmail.com>
2024-02-06 07:23:00 +0530

    mostly done

--- app.js
+++ app.js
@@ -59,7 +59,7 @@ $(blobb).animate({
 
 else {
 
-$('#blob').hidden()
+// $('#blob').hidden()
 
 console.log(
     'success'
@@ -71,16 +71,20 @@ const container = document.querySelector(".container");
 const primaryNav = document.querySelector(".nav__list");
 const toggleButton = document.querySelector(".nav-toggle");
 
-toggleButton.addEventListener("click", () => {
+toggleButton.addEventListener("click", doit ) 
+
+
+function doit() {
     const isExpanded = primaryNav.getAttribute("aria-expanded");
     primaryNav.setAttribute(
         "aria-expanded",
         isExpanded === "false" ? "true" : "false"
     );
-});
+};
 
 container.addEventListener("click", (e) => {
     if (!primaryNav.contains(e.target) && !toggleButton.contains(e.target)) {
+        console.log(e.target)
         primaryNav.setAttribute("aria-expanded", "false");
     }
 });
